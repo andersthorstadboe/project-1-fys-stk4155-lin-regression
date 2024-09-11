4ff75678794ad0e89715af8c4d2302651adac01d
anders_tb <andetb@math.uio.no>
2024-09-11 20:17:54 +0200

    Files for backup

--- reg_functions.py
+++ reg_functions.py
@@ -1,11 +1,116 @@
 ### Imports
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
 
-### Functions
+### Metric functions
 
-def OLS(X):
+def mse_own(y_data: np.ndarray, y_model: np.ndarray):
+    """
+    Calculating the mean square error between a data set\n
+    and a prediction model, returning the MSE - score.
+
+    Parameters
+    --------
+    y_data : array
+        Data set
+    y_model : ndarray
+        Prediction data set, model case
+
+    Returns
+    --------
+    float: The mean square error between y_data and y_model
     
-    return 0
+    """
+    return np.sum((y_data - y_model)**2)/(np.size(y_model))
+
+def r2score(y_data: np.ndarray, y_model: np.ndarray):
+    """
+    Calculating the R²-score for y_model compared to y_data.
+
+    Parameters:
+    ---
+    y_data : array
+        Data set
+    y_model : array
+        Prediction model
+    Returns
+    ---
+    float : R²-score for y_model
+
+    """
+    y_mean = np.mean(y_data)
+    return 1.0 - ((np.sum((y_data - y_model)**2))/(np.sum((y_data - y_mean)**2)))
+
+### Regression Functions
+
+def OLS(y_data, X, split=True, scaling=True):
+    """
+    Takes a design matrix, X, and data set, y, performing a\n
+    ordinary least square (OLS) regression analysis on the data set.\n
+    Calculates the MSE and R² - metrics for the resulting prediction model
+    
+    Parameters
+    ---
+    y_data : ndarray, n...
+        Data set
+    X : ndarray, n...
+        Design matrix
+    split : bool
+        True/1 for splitting the data into test and training data\n
+        False/0 for doing analysis on entire data set
+    
+    Returns
+    ---
+    ndarray : predicted model for plotting
+
+    """
+    ## Splitting and scaling conditions
+    if split == True:
+        X_train, X_test, y_train, y_test = train_test_split(X,y_data,test_size=.2)
+
+    else:
+        y_train = y_data; y_test = y_data
+        X_train = X; X_test = X
+    
+    if scaling == True:
+            # Scaling with the mean value of columns of X
+            y_train_mean = np.mean(y_train)
+            X_train_mean = np.mean(X_train,axis=0)
+            y_train_s = y_train - y_train_mean
+            X_train_s = X_train - X_train_mean 
+            X_test_s  = X_test  - X_train_mean
+    else:
+            y_train_mean = np.mean(y_train)
+            X_train_mean = np.mean(X_train,axis=0)
+            y_train_s = y_train
+            X_train_s = X_train
+            X_test_s  = X_test
+
+    ## Optimization
+    # Optimizing with training data
+    beta_ols = (np.linalg.inv(X_train_s.T @ X_train_s) @ X_train_s.T @ y_train_s)
+    print(beta_ols)
+    # Calculating the intercept
+    intcept_ols = np.mean(y_train_mean - X_train_mean @ beta_ols)
+
+    # Predictions, including the intercept
+    y_ols_train = X_train_s @ beta_ols + intcept_ols
+    y_ols_test  = X_test_s @ beta_ols + intcept_ols
+
+    mse_ols = [mse_own(y_train,y_ols_train),mse_own(y_test,y_ols_test)]
+    mse_sk = [mean_squared_error(y_train,y_ols_train),mean_squared_error(y_test,y_ols_test)]
+
+    r2s_ols = [r2score(y_train,y_ols_train),r2score(y_test,y_ols_test)]
+    r2s_sk = [r2_score(y_train,y_ols_train),r2_score(y_test,y_ols_test)]
+
+    print('Error metrics')
+    print('MSE (Own):\nTraining: %g\nTest: %g' %(mse_ols[0],mse_ols[1]))
+    print('MSE (Scikit):\nTraining: %g\nTest: %g' %(mse_sk[0],mse_sk[1]))
+    print('R² (Own):\nTraining: %g\nTest: %g' %(r2s_ols[0],r2s_ols[1]))
+    print('R² (Scikit):\nTraining: %g\nTest: %g' %(r2s_sk[0],r2s_sk[1]))
+
+    return y_ols_train,y_ols_test, [intcept_ols,beta_ols]
 
 def Ridge(X):
 
@@ -15,12 +120,3 @@ def Lasso(X):
 
     return 0
 
-
-def mse_own(y_data, y_model):
-    return np.sum((y_data - y_model)**2)/(np.size(y_model))
-
-def poly_model_1d(x,poly_deg):
-    X = np.zeros(len(x),poly_deg)
-    for p_d in range(1, poly_deg+1):
-        X[:,p_d-1] = x**p_d
-    return X
--- support_funcs.py
+++ support_funcs.py
@@ -1,14 +1,106 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def Franke(x,y):
-    """Returns the Franke function for x and y arrays
-       as f(x,y)
+### Test functions
+
+def Franke(x,y,x_noise,y_noise,noise=0.0):
+   """
+   Returns the Franke function on a mesh grid (x,y)\n
+   as test function, with or without noise added
+   
+   Parameters
+   --------
+   x, y : array, n x m
+      Mesh grid data points
+   x_noise, y_noise : array, n x m
+      Mesh grid noise data points
+   noise : float
+      Scale for the amount of noise added to the output,\n
+      same on both axis x and y. Default = 0.0
+
+   Returns
+   --------
+   ndarray : 2D-function representing the Franke function
+
+   """
+   p1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+   p2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+   p3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+   p4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+
+   return p1 + p2 + p3 + p4 + noise*x_noise + noise*y_noise
+
+def exp1D(x,x_noise,a,b,noise=0.0):
+   """
+   Returns a 1D exponential test function,\n
+   a*exp(-x²) + b*exp(-(x-2)²) + noise\n
+   for a ndarray x, with or without noise given as ndarray x_noise
+
+   Parameters
+   --------
+   x : ndarray, n
+      Data points
+   x_noise : ndarray, n
+      Noise data array
+   a, b : float
+      Scale of first and second exponential term
+   noise : float
+      Scale for the amount of noise added to the output. Default = 0.0
+
+   Returns
+   --------
+   ndarray : 1D exponential function
+   """
+   return a*np.exp(-x**2) + b*np.exp(-(x-2)**2) + noise*x_noise
+
+def exp2D(x,y,x_noise,y_noise,a,b,noise=0.0):
+   """
+   Returns a 2D exponential test function,\n
+
+   for a mesh grid (x,y) with or without noise 
+   from mesh grid (x_noise, y_noise)
+
+   Parameters
+   ---
+   x, y : array, n x m
+      Mesh grid data points
+   x_noise, y_noise : array, n x m
+      Mesh grid noise data points
+   a, b : float
+      Scale of first and second exponential term
+   noise : float
+      Scale for the amount of noise added to the output,\n
+      same on both axis x and y. Default = 0.0
+   
+   Returns
+   ---
+   ndarray : 2D exponential function
+   """
+   p1 = a*np.exp(-(x**2 + y**2)) + b*np.exp(-(x-2)**2 - (y-2)**2)
+
+   return p1 + noise*x_noise + noise*y_noise
+
+### Other supporting functions
+
+def poly_model_1d(x: np.ndarray,poly_deg: int):
     """
-    p1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    p2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    p3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    p4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    Returning a design matrix for a polynomial of a given degree in\n
+    one variable, x. Starts at i = 1, so intercept column is ommitted\n 
+    from design matrix\n
 
-    return p1 + p2 + p3 + p4
-
+    Parameters
+    --------
+    x : array
+        1d-numpy array, dimension n\n
+    poly_deg : int
+        degree of the resulting polynomial to be modelled, p
+    
+    Returns
+    --------
+    X : array, n x p 
+        Design matrix of dimension n x p
+    """
+    X = np.zeros((len(x),poly_deg))
+    for p_d in range(1, poly_deg+1):
+        X[:,p_d-1] = x**p_d
+    return X
